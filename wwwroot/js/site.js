6e34c6343ff8bdedcc4515b607a8157bb3a53729
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-04-05 18:05:51 +0300

    Added loader spinner

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,8 +7,11 @@ $(() => {
     const $spinner = $("i#spinner");
 
     function loadList() {
+        toggleSpinner();
         $.get("/?handler=ProcessList").done((list) => {
             $processTableBodyPlaceholder.html(list);
+        }).always(() => {
+            toggleSpinner();
         });
     }
 
@@ -27,12 +30,13 @@ $(() => {
         2000);
 
     $("button[data-toggle='sort-process-list']").on("click",
-        function(e) {
+        function() {
             toggleSpinner();
             const index = parseInt($(this).parent().data("action-index"));
             const url = $(this).data("url");
             $.get(url, { "index": index }).done(() => {
                 loadList();
+            }).always(() => {
                 toggleSpinner();
             });
         });
@@ -40,42 +44,54 @@ $(() => {
     $processTableBodyPlaceholder.on("click",
         "button[data-toggle='remove-process']",
         function() {
+            toggleSpinner();
             const id = getProcessIdFromParents($(this));
             const url = $(this).data("url");
             $.get(url, { "id": id }).done(() => {
                 loadList();
+            }).always(() => {
+                toggleSpinner();
             });
         });
 
     $processTableBodyPlaceholder.on("click",
         "button[data-toggle='open-folder']",
         function() {
+            toggleSpinner();
             const id = getProcessIdFromParents($(this));
             const url = $(this).data("url");
             $.get(url, { "id": id }).done(() => {
                 loadList();
+            }).always(() => {
+                toggleSpinner();
             });
         });
     
     $processTableBodyPlaceholder.on("click",
         "button[data-toggle='module-list-modal']",
         function() {
+            toggleSpinner();
             const id = getProcessIdFromParents($(this));
             const url = $(this).data("url");
             $.get(url, { "id": id }).done((modal) => {
                 $modalPlaceholder.html(modal);
                 $modalPlaceholder.find(".modal").first().modal("show");
+            }).always(() => {
+                toggleSpinner();
             });
         });
 
     $processTableBodyPlaceholder.on("click",
         "button[data-toggle='thread-list-modal']",
         function() {
+            toggleSpinner();
             const id = getProcessIdFromParents($(this));
             const url = $(this).data("url");
             $.get(url, { "id": id }).done((modal) => {
                 $modalPlaceholder.html(modal);
                 $modalPlaceholder.find(".modal").first().modal("show");
+            }).always(() => {
+                toggleSpinner();
             });
         });
 
